4762f39db7eddca29820213d3ce09d627c0f3571
sharmavivek1402 <sharmavivek1402@gmail.com>
2026-08-30 19:56:17 +0530

    Login class Added

---
+++ TestCICD/Screens/View/LoginViewController.swift
@@ -0,0 +1,24 @@
+//
+//  LoginViewController.swift
+//  TestCICD
+//
+//  Created by APPLE on 30/08/26.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    @IBOutlet weak var textEmail: UITextField!
+
+    @IBOutlet weak var textPassword: UITextField!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func loginAction(_ sender: Any) {
+        
+    }
+}
